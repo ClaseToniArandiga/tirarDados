e7e5969a1151410d3a1eecb3080fe63693472175
Toni Ferra <toniferra@Tonis-MacBook-Pro.local>
2025-10-20 17:43:06 +0200

    refactorización de objeto Dado en sustitución de Funciones

--- js/main.js
+++ js/main.js
@@ -1,41 +1,59 @@
-// Encontrar con los ID del HTML
-const zonaDado = document.getElementById("zonaDado")
-const boton = document.getElementById("boton")
+// WEBapp: Simulador de Dados (MVP)
+// Versión: 1.0
+// MVP = Producto Mínimo Viable = Tirar dado 6 caras
+
+// Objeto DADO
+let dado = {
+    // Propiedades
+    cantosRedondeados: true,
+    material: ["plástico", "madera", "metal", "resina"],
+    color: "blanco",
+    opacidad: 1,
+    caras: [4, 6, 8, 10, 12, 20, 100],
+    grabado: false,
+    tamano2: {
+        ["pequeño"]: "2cm",
+        ["standard"]: "3cm",
+        ["grande"]: "5cm",
+    },
+    peso: ["ligero", "standard", "pesado"],
+    marca: [
+        "Hasbro",
+        "Chessex",
+        "Koplow",
+        "GameScience",
+        "WizDice",
+        "Q-Workshop",
+        "Die Hard Dice",
+    ],
+    // Métodos
+    tirar: function (caras = 6) {
+        // si las caras no coinciden con las caras definidas en la propiedad caras, devolver un mensaje de error
+        if (!this.caras.includes(caras)) {
+            return "Error: El dado no tiene " + caras + " caras.";
+        }
+        return Math.floor(Math.random() * caras) + 1;
+    },
+};
+
+// Conexiones con HTML
+const zonaDado = document.getElementById("zonaDado"); // <div></div>
+const boton = document.getElementById("boton"); // <a></a>
+
+const posicionesDado = new Map([
+    [1, "0px center"],
+    [2, "-350px center"],
+    [3, "-700px center"],
+    [4, "-1060px center"],
+    [5, "-1450px center"],
+    [6, "-1800px center"],
+]);
 
 
-// Función que se encarga de enseñar el resultado del dado en el HTML
-function tirarDado() {
-    // Llamar a la función dadoDeSeis para obtener el resultado
-    const resultado = dadoDeSeis();
-    // insertar un CSS en una etiqueta HTML ID zonaDado
 
-    // Creación de un constante Mapa, de todos los valores de posición del dado
-    const posicionesDado = new Map([
-        [1, "0px center"],
-        [2, "-350px center"],
-        [3, "-700px center"],
-        [4, "-1060px center"],
-        [5, "-1450px center"],
-        [6, "-1800px center"]
-    ]);
-
-    // Comparar el resultado si es igual que el key del Mapa y insertar el value en el CSS de zonaDado
-    if (posicionesDado.has(resultado)) {
-        zonaDado.style.backgroundPosition = posicionesDado.get(resultado);
-    }
-
-
-
-    // Función que ella sola, ejecute al llamarla una tirada de dados de 6 caras
-    function dadoDeSeis() {
-        // Generar un número aleatorio entre 1 y 6
-        const resultado = Math.floor(Math.random() * 6) + 1;
-        return resultado
-    }
-}
-
-
-// Pulsar el boton y que se ejecute la función
+// Botón para tirar el dado
 boton.addEventListener("click", function () {
-    tirarDado()
-})
+    // tirarDado();
+    const resultado = dado.tirar();
+    zonaDado.style.backgroundPosition = posicionesDado.get(resultado);
+});
