be55bfe5f73b7463d27f2bdf9e54b99e0feeb298
Toni Ferra <70471508+toniferra-dev@users.noreply.github.com>
2025-10-30 18:01:27 +0100

    finalizados los GRID de css, y todo ok, menos el h1 clic

---
+++ js/main-old.js
@@ -0,0 +1,54 @@
+// WEBapp: Simulador de Dados (MVP)
+// Versión: 1.0
+// MVP = Producto Mínimo Viable = Tirar dado 6 caras
+
+// Objeto DADO
+let dado = {
+    // Propiedades
+    cantosRedondeados: true,
+    material: ["plástico", "madera", "metal", "resina"],
+    color: "blanco",
+    opacidad: 1,
+    caras: [4, 6, 8, 10, 12, 20, 100],
+    grabado: false,
+    tamano2: {
+        ["pequeño"]: "2cm",
+        ["standard"]: "3cm",
+        ["grande"]: "5cm",
+    },
+    peso: ["ligero", "standard", "pesado"],
+    marca: [
+        "Hasbro",
+        "Chessex",
+        "Koplow",
+        "GameScience",
+        "WizDice",
+        "Q-Workshop",
+        "Die Hard Dice",
+    ],
+    // Métodos
+    tirar: function (caras = 6) {
+        // si las caras no coinciden con las caras definidas en la propiedad caras, devolver un mensaje de error
+        if (!this.caras.includes(caras)) {
+            return "Error: El dado no tiene " + caras + " caras.";
+        }
+        return Math.floor(Math.random() * caras) + 1;
+    },
+};
+
+// Conexiones con HTML
+const zonaDado = document.getElementById("zonaDado"); // <div></div>
+
+
+
+// Botón para tirar el dado
+zonaDado.addEventListener("click", function () {
+    // tirarDado();
+    const resultado = dado.tirar();
+    zonaDado.children[0].textContent = resultado;
+    // rotación del zonaDado
+    zonaDado.style.transform = "rotate(" + (Math.random() * 90) + "deg)";
+});
+
+
+let dadoDeDiezcarasCantosRectos = {...dado, caras: 10, cantosRedondeados: false};
--- js/main.js
+++ js/main.js
@@ -1,54 +1,84 @@
-// WEBapp: Simulador de Dados (MVP)
-// Versión: 1.0
-// MVP = Producto Mínimo Viable = Tirar dado 6 caras
+// Refactor 29/10/2025:
+// Construir clase Dado de 6 caras
+// Crear Herencia para los otros dados, 4, 8, 10, 12, 20, 100 caras
 
-// Objeto DADO
-let dado = {
-    // Propiedades
-    cantosRedondeados: true,
-    material: ["plástico", "madera", "metal", "resina"],
-    color: "blanco",
-    opacidad: 1,
-    caras: [4, 6, 8, 10, 12, 20, 100],
-    grabado: false,
-    tamano2: {
-        ["pequeño"]: "2cm",
-        ["standard"]: "3cm",
-        ["grande"]: "5cm",
-    },
-    peso: ["ligero", "standard", "pesado"],
-    marca: [
-        "Hasbro",
-        "Chessex",
-        "Koplow",
-        "GameScience",
-        "WizDice",
-        "Q-Workshop",
-        "Die Hard Dice",
-    ],
-    // Métodos
-    tirar: function (caras = 6) {
-        // si las caras no coinciden con las caras definidas en la propiedad caras, devolver un mensaje de error
-        if (!this.caras.includes(caras)) {
-            return "Error: El dado no tiene " + caras + " caras.";
-        }
-        return Math.floor(Math.random() * caras) + 1;
-    },
-};
+// Clase Dado 6 caras
+class DadoBasico{
+    constructor(tamano, material, cantosRedondeados){
+        this.caras = 6;
+        this.color = "blanco";
+        this.colorLetra = "negro";
+        this.tamano = tamano;
+        this.material = material;
+        this.cantosRedondeados = cantosRedondeados;
+    }
 
-// Conexiones con HTML
-const zonaDado = document.getElementById("zonaDado"); // <div></div>
+    // Método para tirar el dado
+    tirar(){
+        return Math.floor(Math.random() * this.caras) + 1
+    }
+}
+
+// Clase Herencia para el dado de 4 caras
+class Dado4caras extends DadoBasico{
+    constructor(tamano, material, cantosRedondeados){
+        super(tamano, material, cantosRedondeados);
+        this.caras = 4;
+        this.color = "verde";
+        this.colorLetra = "blanco";
+    }
+}
+
+// Clase Herencia para el dado de 8 caras
+class Dado8caras extends DadoBasico{
+    constructor(tamano, material, cantosRedondeados){
+        super(tamano, material, cantosRedondeados);
+        this.caras = 8;
+        this.color = "morado";
+        this.colorLetra = "negro";
+    }
+}
+
+// Clase Herencia para el dado de 10 caras
+class Dado10caras extends DadoBasico{
+    constructor(tamano, material, cantosRedondeados){
+        super(tamano, material, cantosRedondeados);
+        this.caras = 10;
+        this.color = "turquesa";
+        this.colorLetra = "negro";
+    }
+}
+
+// Lógica para crear un dado y tirarlo
 
 
+// Crear la nueva instancia del dado y tirarlo, cuando pulsamos la imagen del dado elegido
+document.getElementById("dado6").addEventListener("click", function(){
+    const dado6 = new DadoBasico("standard", "plástico", true);
 
-// Botón para tirar el dado
-zonaDado.addEventListener("click", function () {
-    // tirarDado();
-    const resultado = dado.tirar();
-    zonaDado.children[0].textContent = resultado;
-    // rotación del zonaDado
-    zonaDado.style.transform = "rotate(" + (Math.random() * 90) + "deg)";
+    const resultado = dado6.tirar();
+    // DOM, crear el dado que es un div, con un h1 dentro con el número
+    // Crear el div del dado
+    const divDado6 = document.createElement("div");
+    divDado6.classList.add("dado","dado6");
+    document.querySelector("main").appendChild(divDado6);
+    // Crear el h1 del número
+    const numeroDado6 = document.createElement("h1");
+    numeroDado6.textContent = resultado;
+    divDado6.appendChild(numeroDado6);
+})
+
+// SOLO tirar los dados que hay creado, tener un botón de tirar dados o mecanismo
+document.getElementById("botonTirar").addEventListener("click", function(){
+    const dados = document.querySelectorAll("h1"); // objeto ARRAY
+    dados.forEach(function(h1DelDado){
+        h1DelDado.textContent = Math.floor(Math.random() * 6) + 1;
+    })
+}); 
+
+// Si hago un clic encima de un dado, y un dado es un div, ese div lo tengo que eliminar del DOM
+document.querySelector("main").addEventListener("click", function(evento){
+    if(evento.target.classList.contains("dado")){
+        evento.target.remove();
+    }
 });
-
-
-let dadoDeDiezcarasCantosRectos = {...dado, caras: 10, cantosRedondeados: false};
